f2f12c25476ae05698fcd5eaf5cd252a89da3eec
93294 <9329496@mail.ru>
2022-11-25 22:16:46 +0300

    Initial commit

---
+++ src/main/java/com/fundamentalsofalgorithmspartone/FundamentalsOfAlgorithmsPartOneApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentalsofalgorithmspartone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsOfAlgorithmsPartOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentalsOfAlgorithmsPartOneApplication.class, args);
+    }
+
+}
