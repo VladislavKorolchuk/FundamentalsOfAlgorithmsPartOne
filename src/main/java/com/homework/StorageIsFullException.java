cee9ee44ab6d0b75ded63e16fcba87a775e98137
93294 <9329496@mail.ru>
2022-11-26 19:55:05 +0300

    Доработка ДЗ

--- src/main/java/com/homework/StorageIsFullException.java
+++ src/main/java/com/homework/StorageIsFullException.java
@@ -3,20 +3,4 @@ package com.homework;
 public class StorageIsFullException extends RuntimeException {
     public StorageIsFullException() {
     }
-
-    public StorageIsFullException(String message) {
-        super(message);
-    }
-
-    public StorageIsFullException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public StorageIsFullException(Throwable cause) {
-        super(cause);
-    }
-
-    public StorageIsFullException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
